e5ccee5b4d81b127903ffc677aebf57293e0d248
suhani.kohli <suhani.kohli.18@gmail.com>
2021-07-08 19:47:24 +0530

    mongo db setup

--- api/api.go
+++ api/api.go
@@ -1,13 +1,33 @@
 package api
 
 import (
+	"context"
+	"encoding/json"
+	"io/ioutil"
 	"net/http"
 
-	"gorm.io/gorm"
+	"go.mongodb.org/mongo-driver/mongo"
 )
 
-func GetItem(db *gorm.DB) http.HandlerFunc {
+type FoodDiary struct {
+	MealName string `json:"_mealName,omitempty" bson:"_mealName,omitempty"`
+	FoodItem string `json:"_foodItem,omitempty" bson:"_foodItem,omitempty"`
+	Date     string `json:"_date,omitempty" bson:"_date,omitempty"`
+}
+
+func CreateItem(collection *mongo.Collection) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		return
+		reqBody, _ := ioutil.ReadAll(r.Body)
+		var foodDiary FoodDiary
+		json.Unmarshal(reqBody, &foodDiary)
+
+		result, err := collection.InsertOne(context.TODO(), foodDiary)
+
+		if err != nil {
+
+			return
+		}
+
+		json.NewEncoder(w).Encode(result)
 	}
 }
